f22f4f783cf3588f039610ecb10357dd69a489be
jonli <jonli@tencent.com>
2019-12-29 15:57:07 +0800

    abilitysystem added

--- Source/GameplayAbilitiesTut/GameplayAbilitiesTutCharacter.cpp
+++ Source/GameplayAbilitiesTut/GameplayAbilitiesTutCharacter.cpp
@@ -9,6 +9,7 @@
 #include "GameFramework/Controller.h"
 #include "GameFramework/SpringArmComponent.h"
 
+#include "AbilitySystemComponent.h"
 //////////////////////////////////////////////////////////////////////////
 // AGameplayAbilitiesTutCharacter
 
@@ -27,23 +28,28 @@ AGameplayAbilitiesTutCharacter::AGameplayAbilitiesTutCharacter()
 	bUseControllerRotationRoll = false;
 
 	// Configure character movement
-	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...	
-	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f); // ...at this rotation rate
+	GetCharacterMovement()->bOrientRotationToMovement = true;				// Character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);	// ...at this rotation rate
 	GetCharacterMovement()->JumpZVelocity = 600.f;
 	GetCharacterMovement()->AirControl = 0.2f;
 
 	// Create a camera boom (pulls in towards the player if there is a collision)
 	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
 	CameraBoom->SetupAttachment(RootComponent);
-	CameraBoom->TargetArmLength = 300.0f; // The camera follows at this distance behind the character	
-	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+	CameraBoom->TargetArmLength = 300.0f;		   // The camera follows at this distance behind the character
+	CameraBoom->bUsePawnControlRotation = true;	// Rotate the arm based on the controller
 
 	// Create a follow camera
 	FollowCamera = CreateDefaultSubobject<UCameraComponent>(TEXT("FollowCamera"));
-	FollowCamera->SetupAttachment(CameraBoom, USpringArmComponent::SocketName); // Attach the camera to the end of the boom and let the boom adjust to match the controller orientation
-	FollowCamera->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+	FollowCamera->SetupAttachment(
+		CameraBoom, USpringArmComponent::SocketName);	// Attach the camera to the end of the boom and let the boom adjust to
+														 // match the controller orientation
+	FollowCamera->bUsePawnControlRotation = false;		 // Camera does not rotate relative to arm
 
-	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character) 
+	// Our ability system component.
+	AbilitySystem = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystem"));
+
+	// Note: The skeletal mesh and anim blueprint references on the Mesh component (inherited from Character)
 	// are set in the derived blueprint asset named MyCharacter (to avoid direct content references in C++)
 }
 
@@ -74,8 +80,10 @@ void AGameplayAbilitiesTutCharacter::SetupPlayerInputComponent(class UInputCompo
 
 	// VR headset functionality
 	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &AGameplayAbilitiesTutCharacter::OnResetVR);
-}
 
+	AbilitySystem->BindAbilityActivationToInputComponent(
+		PlayerInputComponent, FGameplayAbilityInputBinds("ConfirmInput", "CancelInput", "AbilityInput"));
+}
 
 void AGameplayAbilitiesTutCharacter::OnResetVR()
 {
@@ -84,12 +92,12 @@ void AGameplayAbilitiesTutCharacter::OnResetVR()
 
 void AGameplayAbilitiesTutCharacter::TouchStarted(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		Jump();
+	Jump();
 }
 
 void AGameplayAbilitiesTutCharacter::TouchStopped(ETouchIndex::Type FingerIndex, FVector Location)
 {
-		StopJumping();
+	StopJumping();
 }
 
 void AGameplayAbilitiesTutCharacter::TurnAtRate(float Rate)
@@ -120,13 +128,13 @@ void AGameplayAbilitiesTutCharacter::MoveForward(float Value)
 
 void AGameplayAbilitiesTutCharacter::MoveRight(float Value)
 {
-	if ( (Controller != NULL) && (Value != 0.0f) )
+	if ((Controller != NULL) && (Value != 0.0f))
 	{
 		// find out which way is right
 		const FRotator Rotation = Controller->GetControlRotation();
 		const FRotator YawRotation(0, Rotation.Yaw, 0);
-	
-		// get right vector 
+
+		// get right vector
 		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
 		// add movement in that direction
 		AddMovementInput(Direction, Value);
--- Source/GameplayAbilitiesTut/GameplayAbilitiesTutCharacter.h
+++ Source/GameplayAbilitiesTut/GameplayAbilitiesTutCharacter.h
@@ -4,10 +4,39 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "AbilitySystemInterface.h"
+
 #include "GameplayAbilitiesTutCharacter.generated.h"
 
-UCLASS(config=Game)
-class AGameplayAbilitiesTutCharacter : public ACharacter
+class UAbilitySystemComponent;
+
+// Example for an enum the FGameplayAbiliyInputBinds may use to map input to ability slots.
+// It's very important that this enum is UENUM, because the code will look for UENUM by the given name and crash if the UENUM can't
+// be found. BlueprintType is there so we can use these in blueprints, too. Just in case. Can be neat to define ability packages.
+UENUM(BlueprintType)
+enum class AbilityInput : uint8
+{
+	UseAbility1 UMETA(DisplayName = "Use Spell 1"),	// This maps the first ability(input ID should be 0 in int) to the action
+													   // mapping(which you define in the project settings) by the name of
+													   // "UseAbility1". "Use Spell 1" is the blueprint name of the element.
+	UseAbility2 UMETA(DisplayName = "Use Spell 2"),	// Maps ability 2(input ID 1) to action mapping UseAbility2. "Use Spell 2" is
+													   // mostly used for when the enum is a blueprint variable.
+	UseAbility3 UMETA(DisplayName = "Use Spell 3"),
+	UseAbility4 UMETA(DisplayName = "Use Spell 4"),
+	WeaponAbility UMETA(
+		DisplayName = "Use Weapon"),	// This finally maps the fifth ability(here designated to be your weaponability, or
+										// auto-attack, or whatever) to action mapping "WeaponAbility".
+
+	// You may also do something like define an enum element name that is not actually mapped to an input, for example if you have a
+	// passive ability that isn't supposed to have an input. This isn't usually necessary though as you usually grant abilities via
+	// input ID, which can be negative while enums cannot. In fact, a constant called "INDEX_NONE" exists for the exact purpose of
+	// rendering an input as unavailable, and it's simply defined as -1. Because abilities are granted by input ID, which is an int,
+	// you may use enum elements to describe the ID anyway however, because enums are fancily dressed up ints.
+};
+
+UCLASS(config = Game)
+class AGameplayAbilitiesTutCharacter : public ACharacter, public IAbilitySystemInterface
 {
 	GENERATED_BODY()
 
@@ -18,19 +47,28 @@ class AGameplayAbilitiesTutCharacter : public ACharacter
 	/** Follow camera */
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
 	class UCameraComponent* FollowCamera;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Abilities, meta = (AllowPrivateAccess = "true"))
+	UAbilitySystemComponent* AbilitySystem;
+
+	// We add this function, overriding it from IAbilitySystemInterface.
+	UAbilitySystemComponent* GetAbilitySystemComponent() const override
+	{
+		return AbilitySystem;
+	};
+
 public:
 	AGameplayAbilitiesTutCharacter();
 
 	/** Base turn rate, in deg/sec. Other scaling may affect final turn rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseTurnRate;
 
 	/** Base look up/down rate, in deg/sec. Other scaling may affect final rate. */
-	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category=Camera)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera)
 	float BaseLookUpRate;
 
 protected:
-
 	/** Resets HMD orientation in VR. */
 	void OnResetVR();
 
@@ -40,14 +78,14 @@ protected:
 	/** Called for side to side input */
 	void MoveRight(float Value);
 
-	/** 
-	 * Called via input to turn at a given rate. 
+	/**
+	 * Called via input to turn at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
 	void TurnAtRate(float Rate);
 
 	/**
-	 * Called via input to turn look up/down at a given rate. 
+	 * Called via input to turn look up/down at a given rate.
 	 * @param Rate	This is a normalized rate, i.e. 1.0 means 100% of desired turn rate
 	 */
 	void LookUpAtRate(float Rate);
@@ -65,8 +103,13 @@ protected:
 
 public:
 	/** Returns CameraBoom subobject **/
-	FORCEINLINE class USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+	FORCEINLINE class USpringArmComponent* GetCameraBoom() const
+	{
+		return CameraBoom;
+	}
 	/** Returns FollowCamera subobject **/
-	FORCEINLINE class UCameraComponent* GetFollowCamera() const { return FollowCamera; }
+	FORCEINLINE class UCameraComponent* GetFollowCamera() const
+	{
+		return FollowCamera;
+	}
 };
-
